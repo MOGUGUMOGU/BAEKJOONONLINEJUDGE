c451f73d98a49cc789abd198ceb9e1e6c512f6ca
MOGUGUMOGU <null>
2019-08-06 01:33:21 +0000

    10817 done

--- 10817.cpp
+++ 10817.cpp
@@ -3,7 +3,15 @@ using namespace std;
 int main(){
 	int a[3];
 	cin >> a[0] >> a[1] >> a[2];
-	for(int i : a){
-		
+	int temp;
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 2; j++){
+			if(a[j]>a[j+1]){
+				temp = a[j];
+				a[j] = a[j+1];
+				a[j+1] = temp;
+			}
+		}
 	}
+	cout << a[1] << endl;
 }
